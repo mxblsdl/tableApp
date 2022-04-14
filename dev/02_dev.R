6590cccedeea9faecd6875a6a63276f056f092da
mxblsdl <maxblasdel@gmail.com>
2022-04-14 09:26:40 -0700

    add rmarkdown to github action

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -24,7 +24,7 @@ golem::add_module(name = "js_button") # Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
-golem::add_fct("helpers", with_test = TRUE, module = )
+golem::add_fct("helpers", with_test = TRUE)
 golem::add_utils("helpers", with_test = TRUE)
 
 ## External resources
@@ -32,29 +32,21 @@ golem::add_utils("helpers", with_test = TRUE)
 golem::add_js_file("script")
 golem::add_js_handler("handlers")
 golem::add_css_file("custom")
-golem::add_sass_file("custom",   )
+golem::add_sass_file("custom")
 
-golem::sass_template()
 ## Add internal datasets ----
 ## If you have data in your package
 usethis::use_data_raw(name = "my_dataset", open = FALSE)
 
 ## Tests ----
 ## Add one line by test you want to create
-usethis::use_test("app")
+# usethis::use_test("app")
 
 # Documentation
 
 ## Vignette ----
-usethis::use_vignette("tableApp")
-devtools::build_vignettes()
-
-## Code Coverage----
-## Set the code coverage service ("codecov" or "coveralls")
-usethis::use_coverage()
-
-# Create a summary readme for the testthat subdirectory
-covrpage::covrpage()
+# usethis::use_vignette("tableApp")
+# devtools::build_vignettes()
 
 ## CI ----
 ## Use this part of the script if you need to set up a CI
@@ -74,24 +66,6 @@ usethis::use_github_action_check_standard()
 usethis::use_github_action_check_full()
 # Add action for PR
 usethis::use_github_action_pr_commands()
-
-# Travis CI
-usethis::use_travis()
-usethis::use_travis_badge()
-
-# AppVeyor
-usethis::use_appveyor()
-usethis::use_appveyor_badge()
-
-# Circle CI
-usethis::use_circleci()
-usethis::use_circleci_badge()
-
-# Jenkins
-usethis::use_jenkins()
-
-# GitLab CI
-usethis::use_gitlab_ci()
 
 # You're now set! ----
 # go to dev/03_deploy.R
